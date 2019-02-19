120a5e5d6dc1c3afa8ee0b2587e8ec79b36972a5
Vladislav <plusch321@mail.ru>
2019-02-20 00:04:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+module.exports = function getZerosCount(num, base)  {
+
+		function fact (N) {
+			let obj = {};
+			let temp = N;
+			for (let i = 2; i < N; i++) {
+				while(!(temp % i)) {
+					obj[i] ? obj[i]++ : obj[i] = 1;
+					temp /= i;
+				}
+			}
+			if(Object.keys(obj).length) return obj;
+			obj[N] = 1;
+			return obj;
+		}
+
+		let ci = Infinity;
+		let obj = fact(base);
+
+		for (let key in obj) {
+			let temp = num;
+			let count = 0;
+			while(temp) {
+				temp = Math.floor(temp / key);
+				count += temp;
+			}
+			if(ci > count/obj[key]) ci = Math.floor(count/obj[key]);
+		}
+
+		return ci;
+	}
